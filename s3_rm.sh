2ab9e9142c4fb41ee5d78fc7fe2b227d1982406d
pbaranoski <94907262+pbaranoski@users.noreply.github.com>
2025-08-21 13:44:45 -0400

    Add files via upload

---
+++ s3_rm.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/sh
+
+search=$1
+bucket_type=$2
+#search='PartB'
+
+bucket_ptb=idrc-dev-datalake-pdl/pbar/landing/
+bucket_xtr=aws-hhs-cms-eadg-bia-ddom-extracts-nonrpod/xtr/DEV/
+
+bucket=idrc-dev-datalake-pdl/pbar/landing/
+
+
+if [ "${bucket_type}" = "xtr" ]; then
+	bucket=${bucket_xtr}
+	uid=idrcxtr
+
+	# Get list of files in s3 bucket
+	#echo "aws s3 ls s3://${bucket}"
+	aws s3 ls s3://${bucket}  > temp.txt
+
+else
+	# point to config file
+	AWS_CONFIG_FILE=/app/IDRC/PTB/CMS/tmp/aws.cfg
+
+	bucket=${bucket_ptb}
+	uid=idrcptb
+
+	# Get list of files in s3 bucket
+	aws s3 ls s3://${bucket} --profile ${uid}  > temp.txt
+
+fi
+	
+
+# Extract the filenames
+awk '{print $4 }' temp.txt > temp2.txt
+
+# find files that match search criteria
+matches=`grep ${search} temp2.txt` 
+
+# iterate thru files and delete
+for filename in ${matches}
+do
+	if [ "${bucket_type}" = "xtr" ]; then
+		aws s3 rm s3://${bucket}${filename} 
+	else
+		aws s3 rm s3://${bucket}${filename} --profile ${uid}
+	fi
+done
